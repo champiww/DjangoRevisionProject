187abc08b03fd1bd7deea5d23aaed93d8d64e9f8
a24sergiobc <a24sergiobc@iessanclemente.net>
2024-11-27 10:27:13 +0100

    blog finished

--- gameUpdatesBlogApp/apps.py
+++ gameUpdatesBlogApp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AchievementsappConfig(AppConfig):
+class GameupdatesblogappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'achievementsApp'
+    name = 'gameUpdatesBlogApp'
